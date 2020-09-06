3da4415f891187778d760b1bd64a9ce122f00c2d
SGang <valnetine92@gmail.com>
2020-09-06 15:19:58 +0900

    id error fix

--- react/src/components/blog/Board/Unit/index.js
+++ react/src/components/blog/Board/Unit/index.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import styles from './unit.module.css'
+import React from "react";
+import styles from "./unit.module.css";
 
-import { Link } from 'react-router-dom';
-import MetaInfo from '@components/common/MetaInfoCard'
-import EditButtons from './EditButtons'
+import { Link } from "react-router-dom";
+import MetaInfo from "@components/common/MetaInfoCard";
+import EditButtons from "./EditButtons";
 
-export default function Unit({data, id, auth}){
-    //console.log(data, auth)
-    return(
-        <Link to ={`/blog/view/${id}`}>
-            <li className={styles.section}>
-                <div className={styles.textbox}>
-                    <h1 className={styles.title}>{data.title}</h1>
-                    <p className={styles.abstract}>{data.abstract}</p>
-                    <MetaInfo {...data}/>
-                </div>
-                {data.thumbnailUrl &&
-                    <div className={styles.imagebox}>
-                        <img src={data.thumbnailUrl}/>
-                    </div>
-                }
-            {auth.user.id==data.author.id&&<EditButtons id={id} auth={auth}/>}
-            </li>
+export default function Unit({ data, id, auth }) {
+  //console.log(data, auth)
+  return (
+    <li className={styles.section}>
+      <div className={styles.textbox}>
+        <Link to={`/blog/view/${id}`}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.abstract}>{data.abstract}</p>
         </Link>
-    )
+        <MetaInfo {...data} />
+      </div>
+      {data.thumbnailUrl && (
+        <div className={styles.imagebox}>
+          <img src={data.thumbnailUrl} />
+        </div>
+      )}
+      {auth?.user?.id == data.author.id && <EditButtons id={id} auth={auth} />}
+    </li>
+  );
 }
-
--- react/src/components/blog/Board/index.js
+++ react/src/components/blog/Board/index.js
@@ -1,50 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import styles from './index.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-import Unit from './Unit'
-import axios from '@lib/Axios'
+import Unit from "./Unit";
+import axios from "@lib/Axios";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-function Board({auth}){
+function Board({ auth }) {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([])
+  useEffect(() => {
+    (async function reqData() {
+      let req = await axios.get("blog/articles");
+      console.log(req.data);
+      setData(req.data.success);
+    })();
+  }, []);
 
-    useEffect(()=>{
-        (async function reqData(){
-            let req = await axios.get('blog/articles')
-            console.log(req.data)
-            setData(req.data.success)
-        })();        
-    },[])
+  const list = data.map((item) => {
+    return (
+      <Unit
+        key={"article" + item.articleId}
+        data={item.article}
+        id={item.articleId}
+        auth={auth}
+      />
+    );
+  });
 
-    const list = data.map(item=>{
-        return(
-            <Unit key={"article"+item.articleId} 
-                  data={item.article} 
-                  id={item.articleId}
-                  auth={auth}
-            />
-        )
-    })
-
-    return(
-        <div className={styles.board}>
-            <ul className={styles.boardList}>
-                {list}
-            </ul>
-        </div>
-        
-    )
+  return (
+    <div className={styles.board}>
+      <ul className={styles.boardList}>{list}</ul>
+    </div>
+  );
 }
 
-function mapStateToProps(state){
-    return {
-        auth: state.auth
-    }
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
 }
 
-export default connect(
-    mapStateToProps,
-    null
-)(Board)
+export default connect(mapStateToProps, null)(Board);
